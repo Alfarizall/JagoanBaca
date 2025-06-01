272fd3bc6be462024de8968679645b6aebc626c3
Alfarizall <muhammad.alfarizal@mhs.itenas.ac.id>
2025-06-01 18:42:28 +0700

    prototype

---
+++ tokobuku/src/main/java/com/example/tokobuku/TokobukuApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokobuku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokobukuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokobukuApplication.class, args);
+	}
+
+}
