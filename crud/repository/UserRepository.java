1346342df1120ebb326bcf85b3ff1351216eb438
Alfarizall <muhammad.alfarizal@mhs.itenas.ac.id>
2025-06-04 21:25:05 +0700

    Delete crud directory

--- crud/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsernameAndPassword(String username, String password);
-}
