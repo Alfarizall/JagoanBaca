97935929dab8b07a3c05cd9d5fedcc742f086e28
MasBakti1 <baktikomararp@gmail.com>
2025-06-09 00:38:53 +0700

    update daftar user + delete user (exept admin)

--- src/main/java/com/example/tokobuku/controller/AdminController.java
+++ src/main/java/com/example/tokobuku/controller/AdminController.java
@@ -1,31 +1,37 @@
 package com.example.tokobuku.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
-
+import com.example.tokobuku.model.User;
 import com.example.tokobuku.service.BookService;
 import com.example.tokobuku.service.TransactionService;
+import com.example.tokobuku.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
     private final BookService bookService;
     private final TransactionService transactionService;
+    private final UserService userService;
 
     @Autowired
-    public AdminController(BookService bookService, TransactionService transactionService) {
-        this.transactionService = transactionService;
+    public AdminController(BookService bookService, TransactionService transactionService, UserService userService) {
         this.bookService = bookService;
+        this.transactionService = transactionService;
+        this.userService = userService;
     }
 
-    @GetMapping("/admin")
+    @GetMapping
     public String adminHome(Model model) {
         model.addAttribute("books", bookService.getAllBooks());
         model.addAttribute("transactions", transactionService.getAllTransaction());
+        model.addAttribute("users", userService.getAllUsers());
         model.addAttribute("message", "Selamat datang di halaman Admin!");
         return "admin";
     }
 }
-
---
+++ src/main/java/com/example/tokobuku/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.tokobuku.service;
+
+import com.example.tokobuku.model.User;
+import com.example.tokobuku.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
